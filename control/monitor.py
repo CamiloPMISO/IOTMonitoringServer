a6105affa0968586f3867a8ecb1c5dd11b859b4f
cperafan <cperafan@mercadolibre.com.co>
2022-07-16 14:31:59 -0500

    add iluminosity event

--- control/monitor.py
+++ control/monitor.py
@@ -45,7 +45,10 @@ def analyze_data():
         city = item['station__location__city__name']
         user = item['station__user__username']
 
-        if item["check_value"] > max_value or item["check_value"] < min_value:
+        if variable == 'iluminosidad' and item["check_value"] < min_value:
+            alert = True
+            
+        elif item["check_value"] > max_value or item["check_value"] < min_value:
             alert = True
 
         if alert:
